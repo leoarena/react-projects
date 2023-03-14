35dce08f1b2556765e9c87a788ec5cd735351a0c
Leonardo Arena <leonardoarena7@gmail.com>
2023-03-14 17:25:27 -0300

    upgrade to jsx

---
+++ 02-tours/src/Tours.jsx
@@ -0,0 +1,17 @@
+import Tour from "./Tour";
+
+export default function Tours({ tours, removeTour }) {
+  return (
+    <section>
+      <div className="title">
+        <h2>our tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
+        })}
+      </div>
+    </section>
+  );
+}
---
+++ 03-reviews/src/App.jsx
@@ -0,0 +1,15 @@
+import Review from "./Review";
+
+export default function App() {
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>our reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Review />
+      </section>
+    </main>
+  );
+}
